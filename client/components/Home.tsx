dbbf33481dee697aa353759c87579a3fbcad9499
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-03-26 15:55:44 +1300

    Add initial user input text box

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,8 +1,12 @@
 function Home() {
   return (
     <div>
-      <h2>Home</h2>
-      <p>This is your homepage</p>
+      <h2>Hify Everything</h2>
+      <p>In a world where Jared wants to remove the letter h, let's rebel.</p>
+
+      <label htmlFor="userInput">Enter your text here:</label>
+      <br />
+      <textarea id="userInput" name="userInput" />
     </div>
   )
 }
