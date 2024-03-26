481d2710cd04d9cdebce9e8af41f5bbf2df512ef
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-03-26 16:23:16 +1300

    finish MVP

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -1,12 +1,55 @@
+import { useState } from 'react'
+
 function Home() {
+  const [userInput, setUserInput] = useState('')
+  const [hResult, setShowHResult] = useState(false)
+
+  function handleChange(e: React.ChangeEvent<HTMLTextAreaElement>) {
+    setUserInput(e.target.value)
+  }
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    setUserInput(userInput)
+    setShowHResult(true)
+
+    return userInput
+  }
+
+  const splitString = userInput.split('')
+
+  const updatedString = splitString.map((char) => {
+    return char.concat('h')
+  })
+
+  const result = updatedString.join('').toString()
+
   return (
     <div>
       <h2>Hify Everything</h2>
-      <p>In a world where Jared wants to remove the letter h, let's rebel.</p>
+      <p>
+        In a world where Jared wants to remove the letter h, let&apos;s rebel.
+      </p>
 
-      <label htmlFor="userInput">Enter your text here:</label>
-      <br />
-      <textarea id="userInput" name="userInput" />
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <label htmlFor="userInput">Enter your text here:</label>
+        <br />
+        <textarea
+          onChange={(e) => handleChange(e)}
+          id="userInput"
+          name="userInput"
+          value={userInput}
+        />
+        <br />
+        <button>Submit</button>
+      </form>
+
+      {hResult && (
+        <div>
+          <h4>Result: </h4>
+          <p>{result}</p>
+        </div>
+      )}
     </div>
   )
 }
