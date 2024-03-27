804c780de8322a31d1deb81973217a18c51aaa9d
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-03-28 11:13:58 +1300

    style new component

--- client/components/Hacked.tsx
+++ client/components/Hacked.tsx
@@ -1,3 +1,12 @@
 export default function Hacked() {
-  return <p>I've been hacked!</p>
+  return (
+    <div className="hacked">
+      <h1 className="hacked">LUCAS IS COMING FOR YOU M8</h1>
+      <img
+        width={500}
+        src="https://media.tenor.com/a4fSQZ1w-BoAAAAM/police-cars-police-chase.gif"
+        alt="several cop cars after after you, punk"
+      />
+    </div>
+  )
 }
