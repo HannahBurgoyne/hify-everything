6127281ea100ddf033877eaa17a889113e9e7ef5
Hannah Burgoyne <hannah.burgoyne@gmail.com>
2024-03-29 14:57:13 +1300

    refactor

--- client/index.tsx
+++ client/index.tsx
@@ -1,8 +1,7 @@
 import { createRoot } from 'react-dom/client'
-import Home from './components/Home.tsx' // Import your Home component
 import App from './components/App.tsx'
 
 document.addEventListener('DOMContentLoaded', () => {
   const rootElement = document.getElementById('app') as HTMLElement
-  createRoot(rootElement).render(<App />) // Render your Home component
+  createRoot(rootElement).render(<App />)
 })
